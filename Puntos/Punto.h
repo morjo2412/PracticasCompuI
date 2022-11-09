9c8d85131e3d22679b8ba6c27f657d0327341f21
Jorge Montes <72566782+morjo2412@users.noreply.github.com>
2022-11-09 15:35:35 -0600

    Create Punto.h

---
+++ Puntos/Punto.h
@@ -0,0 +1,24 @@
+#ifndef PUNTO_H
+#define PUNTO_H
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+class Punto2D
+{
+	public:
+		void SetPosicion(float x1,float y1);
+		void Trasladar(float temp1,float temp2);
+		void RotarRespectoAlOrigen(float temp1);
+		void Escalar(float temp1,float temp2);
+		
+		float GetX();
+		float GetY();
+		
+	private:
+		float x;
+		float y;
+};
+
+#endif
